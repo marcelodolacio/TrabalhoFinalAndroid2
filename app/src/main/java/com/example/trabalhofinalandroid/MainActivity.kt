9b7b2a832f86a051f95e98ba87058097dcdf5615
marcelodolacio <marcelodolacio@gmail.com>
2021-02-09 22:04:05 -0300

    autenticação firebase

---
+++ app/src/main/java/com/example/trabalhofinalandroid/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.trabalhofinalandroid
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bt_Login.setOnClickListener {
+            login();
+        }
+
+        tv_Cadastro.setOnClickListener {
+            registro();
+        }
+
+        tv_esqueciSenha.setOnClickListener {
+            esqueciSenha();
+        }
+    }
+
+    fun login(){
+
+        val email = ti_Email.text.toString();
+        val senha = ti_Senha.text.toString();
+
+        if(email.isNotBlank() && senha.isNotBlank()){
+            Toast.makeText(this, "FUNCIONOU",Toast.LENGTH_LONG).show();
+        }
+
+        val autenticacao = FirebaseAuth.getInstance();
+        val operacao = autenticacao.signInWithEmailAndPassword(email,senha);
+        operacao.addOnCompleteListener { resultado ->
+            if(resultado.isSuccessful){
+                // intents
+                val navegaParaTelaPrincipal = Intent(this, HomeActivity::class.java);
+                startActivity(navegaParaTelaPrincipal);
+            }else{
+                Toast.makeText(this, "Erro: Usuário ou senha inexistentes",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    fun esqueciSenha(){
+        val navegaParaEsqueciSenha = Intent(this, EsqueciSenhaActivity::class.java);
+        startActivity(navegaParaEsqueciSenha);
+    }
+
+    fun registro(){
+        val navegaParaTelaCadastro = Intent(this, CadastroActivity::class.java);
+        startActivity(navegaParaTelaCadastro);
+    }
+}
